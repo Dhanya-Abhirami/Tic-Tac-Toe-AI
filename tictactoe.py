9d1322cc7163625e69377d02f65cc2be07ba7be8
Dhanya-Abhirami <sdhanyaabhirami@gmail.com>
2020-06-21 12:47:05 +0530

    initialize

--- tictactoe.py
+++ tictactoe.py
@@ -1,17 +1,17 @@
 def evaluate(board):
     # horizontal
-    for i in range(3):
-        if board[i][0]==board[i][1]==board[i][2]:
-            if board[i][0]=='X':
+    for r in range(3):
+        if board[r][0]==board[r][1]==board[r][2]:
+            if board[r][0]=='X':
                 return 10
-            elif board[i][0]=='O':
+            elif board[r][0]=='O':
                 return -10
     # vertical
-    for j in range(3):
-        if board[0][j]==board[1][j]==board[2][j]:
-            if board[0][j]=='X':
+    for c in range(3):
+        if board[0][c]==board[1][c]==board[2][c]:
+            if board[0][c]=='X':
                 return 10
-            elif board[0][j]=='O':
+            elif board[0][c]=='O':
                 return -10
     # diagonal
     if (board[0][0]==board[1][1]==board[2][2]) or (board[0][2]==board[1][1]==board[2][0]):
@@ -22,21 +22,37 @@ def evaluate(board):
     # none
     return 0
 
-def minimax(position,depth,maximisingPlayer):
-    if depth == 0 or game_over:
-        return evaluate(position)
+def gameOver():
+    for r in range(3):
+        for c in range(3):
+            if board[r][c]=='_':
+                return False
+    return True
+
+def minimax(board,depth,maximisingPlayer):
+    evaluation = evaluate(board)
+    if gameOver():
+        return 
     if maximisingPlayer:
         maxEval = - float('inf')
         for child in g[position]:
-           evaluation = minimax(depth-1,False)
+           evaluation = minimax(depth+1,False)
            maxEval = max(maxEval,evaluation)
         return maxEval
     else:
         minEval = float('inf')
         for child in g[position]:
-            evaluation= minimax(depth-1,True)
+            evaluation= minimax(depth+1,True)
             minEval = min(minEval,evaluation)
         return minEval
 
-board = [['X','X','O'],['O','_','_'],['X','O','O']]
+def findOptimalMove():
+    optimalMove = None
+    for move in board:
+        score = minimax()
+        if score>optimalMove:
+            optimalMove = score
+    return optimalMove
+
+board = [['X','O','X'],['O','O','X'],['_','_','_']]
 print(evaluate(board))
